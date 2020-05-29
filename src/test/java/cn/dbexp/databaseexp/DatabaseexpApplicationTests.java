472b3adc8c4e665d757cc4e0930c99af84b53a3e
JulyCTA <284180846@qq.com>
2020-05-29 22:33:39 +0800

    updata

---
+++ src/test/java/cn/dbexp/databaseexp/DatabaseexpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dbexp.databaseexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseexpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
