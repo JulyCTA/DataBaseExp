8f69082eef0fff9b608a10d6261601aa5c265c72
JulyCTA <284180846@qq.com>
2020-05-30 11:37:41 +0800

    updata

--- src/test/java/cn/dbexp/databaseexp/DatabaseexpApplicationTests.java
+++ src/test/java/cn/dbexp/databaseexp/DatabaseexpApplicationTests.java
@@ -1,13 +1,27 @@
 package cn.dbexp.databaseexp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DatabaseexpApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection=dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 
 }
